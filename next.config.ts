f0941a3897e45134b1a559cc4875f986e8c4f087
skyboydev <notnotsky26@gmail.com>
2025-06-29 17:53:42 +0530

    Update next.config.ts to allow postimg

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,15 @@
-import type { NextConfig } from "next";  const nextConfig: NextConfig = {   typescript: {     ignoreBuildErrors: true,   },   eslint: {     ignoreDuringBuilds: true,   }, };  export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["i.postimg.cc"],
+  },
+};
+
+export default nextConfig;
