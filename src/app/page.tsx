bd9afa81192d0e64518ba4cbc915ecefee805cc7
Aqua <notnotsky26@gmail.com>
2025-06-29 05:25:39 +0000

    fixed lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import Header from "../components/ui/header";
 import { Button } from "../components/ui/button";
